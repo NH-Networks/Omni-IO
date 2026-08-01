2a6a503bfb78009cfc2773653442e94b35f357dc
NH-Networks <25506868+NH-Networks@users.noreply.github.com>
2026-08-02 01:41:29 +0200

    Fix build error by removing missed setRepeatOnNoResponse implementation

--- src/iohcRemote1W.cpp
+++ src/iohcRemote1W.cpp
@@ -1141,18 +1141,6 @@ const std::vector<iohcRemote1W::remote>& iohcRemote1W::getRemotes() const {
         return true;
     }
 
-    bool iohcRemote1W::setRepeatOnNoResponse(const std::string &description, bool repeatOnNoResponse) {
-        auto it = std::find_if(remotes.begin(), remotes.end(), [&](const remote &e) {
-            return e.description == description;
-        });
-        if (it == remotes.end()) {
-            Serial.printf("Device %s not found\n", description.c_str());
-            return false;
-        }
-        it->repeatOnNoResponse = repeatOnNoResponse;
-        save();
-        return true;
-    }
 
     void iohcRemote1W::updatePositions() {
         for (auto &r : remotes) {
